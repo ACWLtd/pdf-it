833da383f912e3a7160761ff00bbf754ff516bce
James Ilaki <james@acw.uk.com>
2017-04-18 17:17:03 +0100

    Don't unlink. Just for a laugh. LOL

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -23,8 +23,12 @@ class PDFController extends Controller
             if ( $exec && file_exists($file) ) {
                 header('Content-type: application/octet-stream');
                 header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($file));
                 readfile($file);
-                unlink($file);
+//                unlink($file);
                 exit;
             }
         }
