2390fd3b09b49c2e33221f59395df5df015e00ae
James Ilaki <james@acw.uk.com>
2017-04-20 17:34:20 +0100

    Didn't we say breathe?

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -19,7 +19,7 @@ class PDFController extends Controller
      */
     protected function nowMake($url, $file)
     {
-        return shell_exec("xvfb-run wkhtmltopdf -T 5 -B 5 -L 5 -R 5 $url $file");
+        return shell_exec("xvfb-run wkhtmltopdf -T 5 -B 5 -L 10 -R 10 $url $file");
     }
 
     /**
